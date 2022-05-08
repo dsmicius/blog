8b80fb05919542ede357c2e0be0fda1eb6d4d477
Dominykas <dsmicius@gmail.com>
2022-05-08 23:15:42 +0300

    add date to db

---
+++ blog/src/main/resources/db/initial-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO BLOG (id, blogId, subject, description, createDate,updateDate,deleteDate,author,status)
+VALUES
+    (1, '8bf0860a-59ba-446b-86dc-92d58a4e1ec3', 'Trumpas aprasymas Nr. 1', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est id sapiente totam! Ad eum expedita impedit soluta vero voluptas! Aliquam deserunt earum magnam molestiae nemo omnis quisquam reprehenderit totam vero.', '2022-01-01', null, null, 'Dominykas', 'Active'),
+    (2, '8bf0860a-59ba-446b-86dc-92d58a4e1ec4', 'Trumpas aprasymas Nr. 2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est id sapiente totam! Ad eum expedita impedit soluta vero voluptas! Aliquam deserunt earum magnam molestiae nemo omnis quisquam reprehenderit totam vero.', '2022-02-15', null, null, 'Ema', 'Active');
