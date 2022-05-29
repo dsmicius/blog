0b0b26d588e74d855362afc5885cce2213a8df49
Dominykas <dsmicius@gmail.com>
2022-05-29 22:25:12 +0300

    fields validation

--- blog/src/main/java/eu/codeacademy/blog/user/controller/UserController.java
+++ blog/src/main/java/eu/codeacademy/blog/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package eu.codeacademy.blog.user.controller;
 
 import eu.codeacademy.blog.user.dto.UserDto;
+import eu.codeacademy.blog.validator.spring.UserValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,18 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping
+    private final UserValidator validator;
+
+    @GetMapping("/register")
     public String getUserForm(Model model) {
         model.addAttribute("userDto", UserDto.builder().build());
 
         return "/user/user";
     }
 
-    @PostMapping
+    @PostMapping("/register")
     public String register(@Valid UserDto userDto, BindingResult errors) {
+        validator.validate(userDto, errors);
         if (errors.hasErrors()) {
             return "/user/user";
         }
