4d6a2ae07d6ed36f83131ac5f07246692da54d46
Dominykas <dsmicius@gmail.com>
2022-06-03 11:39:28 +0300

    read users from db

--- blog/src/main/java/eu/codeacademy/blog/config/SecurityConfig.java
+++ blog/src/main/java/eu/codeacademy/blog/config/SecurityConfig.java
@@ -1,14 +1,21 @@
 package eu.codeacademy.blog.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
+import javax.sql.DataSource;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -44,7 +51,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("admin@blog.lt")
                     .password("{bcrypt}$2a$10$Y3GKDQIRG9CXCf8dLH836es1/xI9NSOQ1nUuIuZJXgwvUgBytfcOO")
                     .roles("USER","ADMIN");
-
+        auth
+           .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT email AS username, password, TRUE AS enabled FROM users WHERE email = ?")
+                .authoritiesByUsernameQuery("SELECT email AS username, 'USER' AS authority FROM users WHERE email = ?")
+                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
     }
-
 }
