0635c9fb989a940f84c3a676e04f0288b3cbe0a1
Dominykas <dsmicius@gmail.com>
2022-06-09 09:44:40 +0300

    changed in initial-data.sql admin and user name, surname

--- blog/src/main/resources/db/initial-data.sql
+++ blog/src/main/resources/db/initial-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (name,surname,email,password,zip_code,phone_number)
-VALUES ('Vardenis', 'Pavardenis','blog@blog.lt','{bcrypt}$2a$10$SGc9uu/KSfGl/452i94kLOINr.eUyREI/HZKJhoEgLwI8BaNlpZ8C','LT-12345','861012345'),
-('Useris', 'Userenis','user@blog.lt','{bcrypt}$2y$10$W2cZU7MWzkAvtw/znuvKgu1lwLlS1x7DZvXrMTwCyvZ9xYRzi9yAK','LT-12345','861012345');
+VALUES ('Admin', 'Administrator','blog@blog.lt','{bcrypt}$2a$10$SGc9uu/KSfGl/452i94kLOINr.eUyREI/HZKJhoEgLwI8BaNlpZ8C','LT-12345','861012345'), --pass -> blog
+('User', 'User','user@blog.lt','{bcrypt}$2y$10$W2cZU7MWzkAvtw/znuvKgu1lwLlS1x7DZvXrMTwCyvZ9xYRzi9yAK','LT-12345','861012345'); --pass -> user
 
 INSERT INTO authority (name)
 VALUES ('ADMIN'),
