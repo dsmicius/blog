45d5d80001ff8352f48bb66b1558bb8d875e2735
Dominykas <dsmicius@gmail.com>
2022-07-02 12:45:27 +0300

    save file to folder

--- blog/blog-api/src/main/java/eu/codeacademy/blog/api/service/FileService.java
+++ blog/blog-api/src/main/java/eu/codeacademy/blog/api/service/FileService.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 @Slf4j
@@ -16,8 +17,13 @@ public class FileService {
 
     public void saveFile(MultipartFile file) {
         createDirectory();
-
-
+        try {
+            Path filePathWithFileName = fileLocation.resolve(file.getOriginalFilename());
+            Files.copy(file.getInputStream(), filePathWithFileName, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            log.error("Cannot create file", e);
+            e.printStackTrace();
+        }
     }
 
     private void createDirectory() {
