aadb3ce8a0f1597a26150e4071b2d570cc6a25e2
Dominykas <dsmicius@gmail.com>
2022-07-02 13:10:51 +0300

    added FileSizeLimitExceededException advice and ExceptionResponse dto

---
+++ blog/blog-api/src/main/java/eu/codeacademy/blog/api/advice/ApiExceptionHandlerAdvice.java
@@ -0,0 +1,19 @@
+package eu.codeacademy.blog.api.advice;
+
+import eu.codeacademy.blog.api.dto.ApiExceptionResponse;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandlerAdvice {
+
+    @ExceptionHandler(FileSizeLimitExceededException.class)
+    public ApiExceptionResponse handleFileToLargeException(FileSizeLimitExceededException ex) {
+        return ApiExceptionResponse.builder()
+                .status(HttpStatus.PAYLOAD_TOO_LARGE.value())
+                .message(ex.getMessage())
+                .build();
+    }
+}
