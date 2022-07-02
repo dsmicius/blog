2c758fcb5f66eecf7f99ec7a06960ee709348e42
Dominykas <dsmicius@gmail.com>
2022-07-02 18:26:49 +0300

    added to FileController specification

--- blog/blog-api/src/main/java/eu/codeacademy/blog/api/controller/FileController.java
+++ blog/blog-api/src/main/java/eu/codeacademy/blog/api/controller/FileController.java
@@ -2,6 +2,9 @@ package eu.codeacademy.blog.api.controller;
 
 import eu.codeacademy.blog.api.service.FileService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -23,19 +26,34 @@ public class FileController {
     private static final String DOWNLOAD_PATH = FILE_ROOT_PATH + "/download";
     private final FileService fileService;
 
+    @ApiOperation(value = "Save file", httpMethod = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Kai sekmingai sukuriamas failas"),
+            @ApiResponse(code = 403, message = "Neturit permisionu gauti atsakymas"),
+            @ApiResponse(code = 401, message = "Prisijunkite jei norit gauti atsakyma")
+    })
     @PostMapping(UPLOAD_PATH)
     public void saveFile(@RequestParam MultipartFile file) {
         fileService.saveFile(file);
     }
 
+    @ApiOperation(value = "Get file by name", httpMethod = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Kai sekmingai grazinamas failas"),
+            @ApiResponse(code = 403, message = "Neturit permisionu gauti atsakymas"),
+            @ApiResponse(code = 401, message = "Prisijunkite jei norit gauti atsakyma")
+    })
     @GetMapping(DOWNLOAD_PATH)
     public ResponseEntity<Resource> getFileByFileName(@RequestParam String fileName) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
-
         return ResponseEntity.ok()
-                .headers(headers)
+                .headers(getContentDispositionHttpHeader(fileName))
                 .contentType(fileService.getFileMediaType(fileName))
                 .body(fileService.getFile(fileName));
+    }
+
+    private HttpHeaders getContentDispositionHttpHeader(String fileName) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+        return headers;
     }
 }
